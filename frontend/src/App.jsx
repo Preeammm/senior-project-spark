913ef025aab7642283ce8aea2d467c47867756f9
Yaowapa Sabkasedkid <kunprem.2547@gmail.com>
2026-01-21 14:46:09 +0700

    ทำหน้า login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
-  return <Home />
+  return <Login />
 }
 
 export default App
-
-
-
