0d7085d67b286cde93ad8dc398a9e7523da99865
Yaowapa Sabkasedkid <kunprem.2547@gmail.com>
2026-01-21 13:39:42 +0700

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React 🚀</h1>
+      <p>My React app is running correctly</p>
+    </div>
+  )
+}
+
+export default App
+
