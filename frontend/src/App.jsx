d247ec22ba40f749bfc9153f5eb5cdc54521b444
Yaowapa Sabkasedkid <kunprem.2547@gmail.com>
2026-01-21 14:32:09 +0700

    Add Home page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/Home"
 
 function App() {
-  return (
-    <div>
-      <h1>Hello React 🚀</h1>
-      <p>My React app is running correctly</p>
-    </div>
-  )
+  return <Home />
 }
 
 export default App
+
+
 
