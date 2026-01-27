e3dd012a6df6cc1919400e6846e2e826ea10e161
Simon-ict <Apivich.pre@student.mahidol.ac.th>
2026-01-27 18:27:36 +0700

    make project data into another file and group mock data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import me from "./me.js";
+import me from "./mock_data/me.js";
+import project from "./mock_data/project.js";
 
 
 const app = express();
@@ -21,47 +22,7 @@ app.get("/api/me", (req, res) => {
 });
 
 app.get("/api/projects", (req, res) => {
-  res.json([
-    {
-      id: "p1",
-      projectName: "Final Project",
-      courseName:
-        "ITCS495 - Special Topics in Databases and Intelligent Systems",
-      yearSemester: "Year 4 Semester 1",
-      type: "Group",
-      competencyTags: ["Tag 1", "Tag 2", "Tag 3"],
-      relevancePercent: 90,
-      materials: [
-        { name: "Report.pdf", url: "http://localhost:3000/static/report.pdf" },
-        {
-          name: "Instruction.pdf",
-          url: "http://localhost:3000/static/instruction.pdf",
-        },
-      ],
-    },
-
-    // 🔹 NEW DATA (แถวที่ 2)
-    {
-      id: "p2",
-      projectName: "Database Mini Project",
-      courseName: "ITCS241 - Database Management Systems",
-      yearSemester: "Year 2 Semester 1",
-      type: "Individual",
-      competencyTags: ["Tag 1", "Tag 2"],
-      relevancePercent: 85,
-      materials: [
-        { name: "Report.pdf", url: "http://localhost:3000/static/report2.pdf" },
-        {
-          name: "Instruction.pdf",
-          url: "http://localhost:3000/static/instruction2.pdf",
-        },
-        {
-          name: "query.sql",
-          url: "http://localhost:3000/static/query.sql",
-        },
-      ],
-    },
-  ]);
+  res.json(project);
 });
 
 app.listen(3000, () => console.log("API on http://localhost:3000"));
